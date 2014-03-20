b2351d7feaf66906601b9b01f14c960ae5c5655a
Darko Krizic <darko@krizic.net>
2014-03-20 10:29:40 +0100

    Tests separated in a specific sequence

--- university-course/university-course-client/src/main/java/com/prodyna/pac/university/course/Course.java
+++ university-course/university-course-client/src/main/java/com/prodyna/pac/university/course/Course.java
@@ -83,3 +83,4 @@ public class Course implements Serializable {
 	}
 
 }
+
--- university-test/src/test/java/com/prodyna/pac/university/course/CourseTest.java
+++ university-test/src/test/java/com/prodyna/pac/university/course/CourseTest.java
@@ -6,11 +6,14 @@ import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 @RunWith(Arquillian.class)
 public class CourseTest {
@@ -27,17 +30,33 @@ public class CourseTest {
     	return wa;
     }
 
-	
+    @Test
+    @InSequence(1)
+    public void emptyDatabase() {
+        List<Course> courses = cs.readCourses();
+        for( Course course : courses ) {
+            cs.deleteCourse( course.getId() );
+        }
+        assertEquals( 0, cs.readCourses().size() );
+    }
+
+    @Test
+    @InSequence(2)
+    public void addCourse() {
+        Course c0 = new Course();
+        c0.setName("Informatics");
+        c0.setDescription("Super course");
+        Course c1 = cs.addCourse(c0);
+        Assert.assertNotNull( c1.getId() );
+        assertEquals( 1, cs.readCourses().size() );
+    }
+
 	@Test
+    @InSequence(3)
 	public void simpleTest() {
-		assertEquals( 0, cs.readCourses().size() );
-		Course c0 = new Course();
-		c0.setName("Informatics");
-		c0.setDescription("Super course");
-		Course c1 = cs.addCourse(c0);
-		Assert.assertNotNull( c1.getId() );
-		assertEquals( 1, cs.readCourses().size() );
-		cs.deleteCourse( c1.getId() );
+        List<Course> courses = cs.readCourses();
+        assertEquals( 1, courses.size() );
+		cs.deleteCourse( courses.get(0).getId() );
 		assertEquals( 0, cs.readCourses().size() );
 	}
 	
